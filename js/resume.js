e826931f759f61e20d37882c4843ea5a0794e416
Dinesh <iamdineshbasnet@gmail.com>
2023-07-13 21:18:44 +0545

    ⚔️  updates in resume page

--- js/resume.js
+++ js/resume.js
@@ -13,13 +13,22 @@ const knowledges = [
 	'Nextjs',
 	'Redux',
 	'Material UI',
+	'Tailwind CSS',
 	'Git',
-    'Formik',
-    'Bootstrap',
-    'jQuery',
-    'GSAP'
+	'Formik',
+	'Bootstrap',
+	'jQuery',
+	'GSAP',
 ];
-
+const certificate_list = [
+	{
+		title: 'Frontend Development',
+		membershipId: 'VRITREACT011820',
+		date: '07 Feb 2023',
+		name: 'Vrit Tech',
+		image: '../assets/certificate1.png',
+	},
+];
 $(document).ready(function () {
 	coding_skills.forEach((skill) => {
 		const skills_content = $(`<div class="skills-content"></div>`);
@@ -41,12 +50,29 @@ $(document).ready(function () {
 		$('.skills-content-wrapper').append(skills_content);
 	});
 
-    knowledges.forEach((knowledge)=>{
-        const knowledge_button = $(`<div class="knowledge-button"></div>`)
-        const text = $(`<p></p>`).text(knowledge)
+	knowledges.forEach((knowledge) => {
+		const knowledge_button = $(`<div class="knowledge-button"></div>`);
+		const text = $(`<p></p>`).text(knowledge);
 
-        knowledge_button.append(text)
+		knowledge_button.append(text);
 
-        $('.knowledges-content-wrapper').append(knowledge_button)
-    })
+		$('.knowledges-content-wrapper').append(knowledge_button);
+	});
+
+	certificate_list.forEach((item) => {
+		const link = $(`<a href=${item.image} download></a>`);
+		const certificate_card = $(`<div class="certificate-card"></div>`)
+		const left = $(`<div class="left"></div>`)
+		const name = $(`<p></p>`).text(item.name)
+		const right = $(`<div class="right"></div>`)
+		const certificate_title =$(`<h4 class="certificate-title"></h4>`).text(item.title)
+		const certificate_membership_id = $(`<p class="certificate-membership-id">Membership id:  <span>${item.membershipId}</span></p>`)
+		const certificate_date = $(`<p class="certificate-date"></p>`).text(item.date)
+
+		left.append(name)
+		right.append(certificate_title, certificate_membership_id, certificate_date)
+		certificate_card.append(left, right)
+		link.append(certificate_card)
+		$('.certificate-wrapper').append(link)
+	});
 });
